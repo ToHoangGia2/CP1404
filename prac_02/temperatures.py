efee7e242e8c2ea9f7388805e02e5f39d730bfde
ToHoangGia <tohoanggia091107@gmail.com>
2025-09-25 14:03:29 +0800

    function convert fahrenheit to celsius

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -15,13 +15,18 @@ def main():
             print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit : "))
-            celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
             print(f"Result: {celsius:.2f} C")
         else:
             print("Invalid option")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
 
 
 def convert_celsius_to_fahrenheit(celsius):
