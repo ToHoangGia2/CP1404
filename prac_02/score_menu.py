0d1233e4579ddc4d68341f5376e6bfd41ac9939e
ToHoangGia <tohoanggia091107@gmail.com>
2025-09-28 15:47:11 +0800

    main function with other function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -6,35 +6,43 @@ def main():
     user_choice = input("Enter your choice: ").upper()
     while user_choice != "Q":
         if user_choice == "G":
-            score = int(input("Enter your score: "))
-            while score < 0 or score > 100:
-                print("Enter a valid score between 0 and 100 inclusive")
-                score = int(input("Enter your score: "))
-
+            score = get_score(score)
         elif user_choice == "P":
             if score == "":
                 print("There is no score")
-            elif score < 0 or score > 100:
-                print("Invalid score")
-            elif score >= 90:
-                print("Excellent")
-            elif score >= 50:
-                print("Passable")
             else:
-                print("Bad")
-
+                print(determine_score(score))
         elif user_choice == "S":
             if score == "":
                 print("There is no score")
             else:
                 print(score * "*")
-
         else:
             print("Invalid choice")
-
         user_choice = input("Enter your choice: ").upper()
 
     print("Thank you!")
+
+
+def determine_score(score):
+    """this function will determine the score"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def get_score(score):
+    """this function will return the valid score"""
+    score = int(input("Enter your score: "))
+    while score < 0 or score > 100:
+        print("Enter a valid score between 0 and 100 inclusive")
+        score = int(input("Enter your score: "))
+    return score
 
 
 main()
