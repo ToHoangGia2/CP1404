bb39e9b9a7065ec22e6444174e6b7bdd2e149297
ToHoangGia <tohoanggia091107@gmail.com>
2025-10-02 10:32:33 +0800

    finished

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+#What did you see on line 1?
+#this random randint random number from 5 to 20 with the smallest number is 5 and highest is 20
+
+#What did you see on line 2?
+#this this one is like randint but an extra thing called step which is the different range between number.
+#In this case the randrange step is 2 therefore the number +2 everytime which make it only print odd number and cant print 4
+
+#What did you see on line 3?
+#this is same at randint too but instead of random number in int stype it change the number to float
+#The lowest number I have seen is 2.5228460094906273 and the highest number is 5.426581031495177
