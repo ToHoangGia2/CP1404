35050a0f3e254020811a591531301c204693200d
ToHoangGia <tohoanggia091107@gmail.com>
2025-10-04 08:49:55 +0800

    fourth programs

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,7 +7,6 @@ out_file.close()
 """2. In the same file, but as if it were a separate program, write code that opens "name.txt" and reads the name (as above) then prints (note the exact output),
 Hi Bob! (or whatever the name is in the file). Do not simply print the user's input variable!
 Use open and close for this question."""
-
 with open("name.txt","r") as in_file:
     print(f"Hi {in_file.read().strip()}!")
 
@@ -16,9 +15,14 @@ with open("name.txt","r") as in_file:
 42
 400
 Write code that opens numbers.txt, reads only the first two numbers, adds them together then prints the result, which should be... 59. Use with instead of open and close for this question."""
-
 with open("numbers.txt","r") as in_file:
     first_number = int(in_file.readline())
     second_number = int(in_file.readline())
 print(first_number+second_number)
 
+"""4. Now write a fourth block of code that prints the total for all lines in numbers.txt. This should work for a file with any number of numbers. Use with instead of open and close for this question."""
+total = 0
+with open("numbers.txt","r") as in_file:
+    for line in in_file:
+        total += int(line)
+print(total)
