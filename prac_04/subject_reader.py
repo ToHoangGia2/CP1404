95e2222636521bc4505bc35f6980054f0fa39642
ToHoangGia <tohoanggia091107@gmail.com>
2025-10-06 16:50:00 +0800

    change the load_data function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -4,7 +4,7 @@ Data file -> lists program
 """
 
 FILENAME = "subject_data.txt"
-
+lines = []
 
 def main():
     data = load_data(FILENAME)
@@ -15,14 +15,15 @@ def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        #print(line)  # See what a line looks like
+        #print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        #print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        #print(parts)  # See if that worked
+        #print("----------")
+        lines.append(parts)
     input_file.close()
 
 
