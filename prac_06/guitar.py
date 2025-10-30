032c4593694f86939fcbd62e44b1d01b2981123d
ToHoangGia <tohoanggia091107@gmail.com>
2025-10-30 11:04:09 +0800

    function get_age

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,3 +1,5 @@
+CURRENT_YEAR=2025
+
 class Guitar:
     def __init__(self,name="", year=0, cost=0):
         """define value"""
@@ -6,10 +8,17 @@ class Guitar:
         self.cost = cost
 
     def __str__(self):
+        """return a string output"""
         return f"{self.name} ({self.year}) : ${self.cost}"
 
+    def get_age(self):
+        """return age of the guitar"""
+        return CURRENT_YEAR - self.year
+
+
+
 def test():
-    guitar = Guitar("Guitar", 20, 10)
+    guitar = Guitar("Guitar", 1977, 10)
     print(guitar)
 
 test()
