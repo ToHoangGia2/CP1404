b34297bf4887c06671a2245aa044ca370b716fde
ToHoangGia <tohoanggia091107@gmail.com>
2025-10-30 10:50:16 +0800

    add comment for programming_language

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,12 +1,15 @@
 class ProgrammingLanguage:
     def __init__(self, name="", typing="", reflection="", year=""):
+        """add value to self"""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Check if self is a dynamic language"""
         return self.typing == "Dynamic"
 
     def __str__(self):
+        """return a string of the language"""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, {self.year}"
