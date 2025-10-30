9b57c0e68c49f7f2d67f55d4b5291d09fb99144e
ToHoangGia <tohoanggia091107@gmail.com>
2025-10-30 11:06:54 +0800

    function is_vintage

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,4 +1,5 @@
 CURRENT_YEAR=2025
+APPROVED_AGE=50
 
 class Guitar:
     def __init__(self,name="", year=0, cost=0):
@@ -15,10 +16,14 @@ class Guitar:
         """return age of the guitar"""
         return CURRENT_YEAR - self.year
 
-
+    def is_vintage(self):
+        """return True if the guitar is vintage"""
+        return self.get_age() >= APPROVED_AGE
 
 def test():
     guitar = Guitar("Guitar", 1977, 10)
     print(guitar)
+    print(guitar.get_age())
+    print(guitar.is_vintage())
 
 test()
