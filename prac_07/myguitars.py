01a58b56688063ae15d89e4ef100564c3c43471d
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-09 11:03:28 +0800

    prompt user for name year and cost

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,6 +1,7 @@
 from prac_07.guitar import Guitar
 
 FILENAME = 'guitars.csv'
+CURRENT_YEAR = 2025
 
 def main():
     """a"""
@@ -9,14 +10,48 @@ def main():
 
     display_guitars(guitars)
 
+    name = input('\nEnter a guitar name if you want to add\nEnter guitar name: ')
+    while name!="":
+        year = get_valid_year()
+        cost = get_valid_cost()
+
+def get_valid_year():
+    is_valid = False
+    while not is_valid:
+        try:
+            year = int(input("Year of guitar: "))
+            while year < 0 or year > CURRENT_YEAR:
+                print("please enter a valid year")
+                year = int(input("Year of guitar: "))
+            is_valid = True
+        except ValueError:
+            print("please enter a valid number")
+    return year
+
+
+def get_valid_cost():
+    is_valid = False
+    while not is_valid:
+        try:
+            cost = float(input("Year of guitar: "))
+            while cost < 0:
+                print("please enter a valid year")
+                cost = float(input("Year of guitar: "))
+            is_valid = True
+        except ValueError:
+            print("please enter a valid number")
+    return cost
+
 
 def display_guitars(guitars):
+    """display guitars"""
     max_name_length = max(len(guitar.name) for guitar in guitars)
     for guitar in guitars:
         print(f"{guitar.name:{max_name_length + 3}}{guitar.year:6}{guitar.cost}")
 
 
 def load_guitars():
+    """load guitars to a list and return that list"""
     guitars = []
     with open(FILENAME) as in_file:
         in_file.readline()
