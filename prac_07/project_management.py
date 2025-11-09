350ce6e01e27c4d0bf1d631870066c1feb48d863
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-09 12:38:27 +0800

    import, Menu, File name

---
+++ prac_07/project_management.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from project import Project
+
+DEFAULT_FILENAME = "projects.txt"
+
+MENU = """- (L)oad projects
+- (S)ave projects
+- (D)isplay projects
+- (F)ilter projects by date
+- (A)dd new project
+- (U)pdate project
+- (Q)uit"""
