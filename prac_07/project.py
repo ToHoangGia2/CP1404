04441a0a576b42208422cf82a07b59baf3c1aca8
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-09 14:53:06 +0800

    add __lt__ for sorting order

--- prac_07/project.py
+++ prac_07/project.py
@@ -9,5 +9,8 @@ class Project:
     def __str__(self):
         return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%")
 
+    def __lt__(self, other):
+        return self.priority < other.priority
+
     def is_complete(self):
         return self.completion_percentage >= 100
