90364299d5f39cbae81cd481c17dfc73eef63a63
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-09 14:41:19 +0800

    add new function to check if project is completed

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,3 +8,6 @@ class Project:
 
     def __str__(self):
         return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%")
+
+    def is_complete(self):
+        return self.completion_percentage >= 100
