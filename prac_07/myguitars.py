d22bbd20b9b1c6a2aad8b5c4ff02418db4d2a9f4
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-09 12:06:07 +0800

    finished and fix all the code

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,18 +4,33 @@ FILENAME = 'guitars.csv'
 CURRENT_YEAR = 2025
 
 def main():
-    """a"""
+    """display all guitars in file"""
     guitars = load_guitars()
     guitars.sort()
 
     display_guitars(guitars)
 
-    name = input('\nEnter a guitar name if you want to add\nEnter guitar name: ')
-    while name!="":
-        year = get_valid_year()
-        cost = get_valid_cost()
+    add_new_guitar()
+
+    guitars = load_guitars()
+    guitars.sort()
+    display_guitars(guitars)
+
+
+def add_new_guitar():
+    """add a new guitar"""
+    with open(FILENAME, 'a') as out_file:
+        name = input('\nEnter a guitar name if you want to add\nEnter guitar name: ')
+        while name != "":
+            year = get_valid_year()
+            cost = get_valid_cost()
+            new_guitar = Guitar(name, year, cost)
+            print(f"{new_guitar.name},{new_guitar.year},{new_guitar.cost}", file=out_file)
+            name = input('\nEnter a guitar name if you want to add\nEnter guitar name: ')
+
 
 def get_valid_year():
+    """prompt user for year and return it"""
     is_valid = False
     while not is_valid:
         try:
@@ -30,13 +45,14 @@ def get_valid_year():
 
 
 def get_valid_cost():
+    """prompt user for cost and return it"""
     is_valid = False
     while not is_valid:
         try:
-            cost = float(input("Year of guitar: "))
+            cost = float(input("Cost of guitar: "))
             while cost < 0:
                 print("please enter a valid year")
-                cost = float(input("Year of guitar: "))
+                cost = float(input("Cost of guitar: "))
             is_valid = True
         except ValueError:
             print("please enter a valid number")
@@ -53,12 +69,12 @@ def display_guitars(guitars):
 def load_guitars():
     """load guitars to a list and return that list"""
     guitars = []
-    with open(FILENAME) as in_file:
-        in_file.readline()
-        for line in in_file:
-            parts = line.strip().split(',')
-            guitar = Guitar(parts[0], parts[1], parts[2])
-            guitars.append(guitar)
+    in_file = open(FILENAME, 'r')
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], parts[1], parts[2])
+        guitars.append(guitar)
+    in_file.close()
     return guitars
 
 
