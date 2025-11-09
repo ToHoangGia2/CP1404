3fcca9a54d994d84b9a39d695e7e23a832d8608d
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-09 13:44:19 +0800

    __str__ function

--- prac_07/project.py
+++ prac_07/project.py
@@ -5,3 +5,6 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%")
