346a1b7ab9b5b1714d6b0d319aa967014f21f72d
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-09 10:06:21 +0800

    load all file

---
+++ prac_07/myguitars.py
@@ -0,0 +1,22 @@
+from prac_07.guitar import Guitar
+
+FILENAME = 'guitars.csv'
+
+def main():
+    """a"""
+    guitars = []
+    with open(FILENAME) as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(',')
+            guitar = Guitar(parts[0], parts[1], parts[2])
+            guitars.append(guitar)
+
+    max_name_length = max(len(guitar.name) for guitar in guitars)
+
+    for guitar in guitars:
+        print(f"{guitar.name:{max_name_length + 3}}{guitar.year:6}{guitar.cost}")
+
+
+if __name__ == '__main__':
+    main()
