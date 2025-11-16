a785d06292ec740912df4e829fbc5d57dfcb8774
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-16 20:59:12 +0800

    finish project

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -9,12 +9,12 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        print('greet')
+        #print('greet')
         #self.root.ids.output_label.text = "Hello "
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
-        self.root.ids.input_miles.text = ""
+        self.root.ids.input_name.text = ""
         self.root.ids.output_label.text = ""
 
 
