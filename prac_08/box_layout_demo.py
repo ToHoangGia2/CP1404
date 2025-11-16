d978d577dc5a266fa2a0f31a0998339f201f60e1
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-16 20:18:17 +0800

    change hello output

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -10,7 +10,8 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):
         print('greet')
-        self.root.ids.output_label.text = "Hello "
+        #self.root.ids.output_label.text = "Hello "
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
 
 BoxLayoutDemo().run()
