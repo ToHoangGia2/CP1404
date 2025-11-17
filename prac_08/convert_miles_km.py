ded726b1ebdd4fbe5b029e6ca54abb5abdea48bf
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-17 11:16:42 +0800

    add string property as output_km

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,7 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
 
 class CovertMilesApp(App):
+    output_km = StringProperty()
+
     def build(self):
         self.title = "Convert Miles and Km"
         self.root = Builder.load_file("convert_miles_km.kv")
