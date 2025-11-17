18394621e63c3bf9eb3ec6e44b23bf3ed51ae4f4
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-17 13:22:39 +0800

    add loop for each name and create separate label for each

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.label import Label
 
 class DynamicLabelApp(App):
     def __init__(self, **kwargs):
@@ -9,3 +10,10 @@ class DynamicLabelApp(App):
     def build(self):
         self.title = "Dynamic Labels Demo"
         self.root = Builder.load_file("dynamic_labels.kv")
+
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+
+
+DynamicLabelApp().run()
