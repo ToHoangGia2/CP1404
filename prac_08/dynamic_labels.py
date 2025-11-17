841328e81b608ad180707dca53d93a276f855202
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-17 13:14:44 +0800

    title of the app and load file

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -2,8 +2,10 @@ from kivy.app import App
 from kivy.lang import Builder
 
 class DynamicLabelApp(App):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.names = ['Tin', 'Ben', 'Random', ':D']
 
     def build(self):
+        self.title = "Dynamic Labels Demo"
+        self.root = Builder.load_file("dynamic_labels.kv")
