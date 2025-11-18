702192b9e0e9f86fc9b5950b019ed98ac6edecc1
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-19 00:27:03 +0800

    play function

--- prac_09/band.py
+++ prac_09/band.py
@@ -6,7 +6,8 @@ class Band:
     def __str__(self):
         return f"{self.name} ({self.members})"
 
-
+    def play(self):
+        return "\n".join(member.play() for member in self.members)
 
     def add(self, member):
         self.members.append(member)
