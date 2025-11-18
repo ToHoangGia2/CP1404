62081a8de89f8dda2e415a2ce8b98d7dbf2fbaf9
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-18 20:06:03 +0800

    __str__

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -8,3 +8,6 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= self.fanciness
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
