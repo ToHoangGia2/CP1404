8d85d1430c02b6be6ef596c6b3daffc0278d54ab
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-19 00:17:38 +0800

    __str__ function

--- prac_09/band.py
+++ prac_09/band.py
@@ -3,5 +3,10 @@ class Band:
         self.name = name
         self.members = []
 
+    def __str__(self):
+        return f"{self.name} ({self.members})"
+
+
+
     def add(self, member):
         self.members.append(member)
