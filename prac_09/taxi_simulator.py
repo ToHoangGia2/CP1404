5472dbeeb8f67cd52135c900bf86bff5e41da1e8
ToHoangGia <tohoanggia091107@gmail.com>
2025-11-20 10:51:34 +0800

    Menu, taxi, current taxi

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,3 +1,10 @@
 from prac_09.silver_service_taxi import SilverServiceTaxi
 from prac_09.taxi import Taxi
 
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+
+    current_taxi = None
